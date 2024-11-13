924136635098ad46f565e2ec5f63b79a97464177
Fatma Birel <fatmabrl11@gmail.com>
2024-11-13 23:23:47 +0300

    Configuration for Delete Directive

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -9,13 +9,18 @@ import {
   Renderer2,
 } from '@angular/core';
 
-import { ProductService } from '../../services/common/models/product.service';
-
 import { MatDialog } from '@angular/material/dialog';
 import {
   DeleteDialogComponent,
   DeleteState,
 } from '../../dialogs/delete-dialog/delete-dialog.component';
+import { HttpClientService } from '../../services/common/http-client.service';
+import {
+  AlertifyService,
+  MessageType,
+  Position,
+} from '../../services/admin/alertify.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 declare var $: any;
 
@@ -26,8 +31,9 @@ export class DeleteDirective {
   constructor(
     private element: ElementRef,
     private _renderer: Renderer2,
-    private productService: ProductService,
-    private dialog: MatDialog
+    private httpClientService: HttpClientService,
+    private dialog: MatDialog,
+    private alertifyService: AlertifyService
   ) {
     const img = _renderer.createElement('img');
     img.setAttribute('src', '../../../../../assets/delete.png');
@@ -38,22 +44,45 @@ export class DeleteDirective {
   }
 
   @Input() id: string;
+  @Input() controller: string;
   @Output() callback: EventEmitter<any> = new EventEmitter();
   @HostListener('click')
   async onclick() {
     this.openDialog(async () => {
       const td: HTMLTableCellElement = this.element.nativeElement;
-      await this.productService.delete(this.id);
-      $(td.parentElement).animate(
-        {
-          opacity: 0,
-          left: '+=50',
-          height: 'toogle',
-        },
-        () => {
-          this.callback.emit();
-        }
-      );
+      this.httpClientService
+        .delete(
+          {
+            controller: this.controller,
+          },
+          this.id
+        )
+        .subscribe((data) => {
+          $(td.parentElement).animate(
+            {
+              opacity: 0,
+              left: '+=50',
+              height: 'toogle',
+            },
+            () => {
+              this.callback.emit();
+              this.alertifyService.message('Ürün başarıyla silinmiştir.', {
+                dismissOthers: true,
+                messageType: MessageType.Success,
+                position: Position.BottomRight,
+                delay: 3,
+              });
+            }
+          ),
+            (errorResponse: HttpErrorResponse) => {
+              this.alertifyService.message('Ürün silinirken beklenmedik bir hatayla karşılaşıldı.', {
+                dismissOthers: true,
+                messageType: MessageType.Error,
+                position: Position.BottomRight,
+                delay: 3,
+              });
+            };
+        });
     });
   }
 
